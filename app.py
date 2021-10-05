56a6be26a3ff1ed1dfedead0521c77f00b07916f
PRAKARN KIDNGUN <51776670+mewprakarn@users.noreply.github.com>
2021-10-05 21:02:53 +0700

    Add Scatter Plot

--- app.py
+++ app.py
@@ -16,17 +16,22 @@ app.config['SECRET_KEY'] = 'mykey'
 def home():
     return render_template("home.html")
 
-@app.route('/d3')
-def d3():
+@app.route('/bar_chart')
+def bar_chart():
     data = [ 35, 21, 38, 77, 32, 44, 47, 80, 37, 50, 62, 49, 92, 63, 62, 72, 63, 157, 83, 65, 103, 90, 87, 183, 86, 109, 108, 95, 72, 92]
-    return render_template("d3.html",mydata=data)
+    return render_template("bar_chart.html",mydata=data)
+
+@app.route('/scatter')
+def scatter():
+    # data = [ 35, 21, 38, 77, 32, 44, 47, 80, 37, 50, 62, 49, 92, 63, 62, 72, 63, 157, 83, 65, 103, 90, 87, 183, 86, 109, 108, 95, 72, 92]
+    return render_template("scatter.html")
 
 @app.route("/altair")
 def altair():
     return render_template("altair.html")
 
 #*Altair Plot
-@app.route('/altair/scatter')
+@app.route('/altair')
 def altair_plot():
     #* Load Data
     url = "https://gist.githubusercontent.com/puripant/857f1981667e8b42da2c72328ba94ead/raw/296d212615ab076254da03573f8f2493007cc76c/medals.csv"
