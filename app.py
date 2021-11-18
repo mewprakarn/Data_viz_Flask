c0269edfca6fdcc39cef907203948d7ad4caa469
PRAKARN KIDNGUN <51776670+mewprakarn@users.noreply.github.com>
2021-11-18 22:51:23 +0700

    update ranking page

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ f = open('static/data/Heatmap_B15112021.json',)
 heatmap2_data = json.load(f)
 f.close()
 
-f = open('static/data/Ranking_v2.json',)
+f = open('static/data/Ranking_v3.json',)
 ranking_data = json.load(f)
 f.close()
 
